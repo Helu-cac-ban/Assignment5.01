c6e116e1276616d0c91778be0a2507a4e1aff0e4
2CucKeo <92835482+Helu-cac-ban@users.noreply.github.com>
2022-11-28 23:11:11 +0700

    Update main.cpp

--- Assignment5.01/22127233-22127477/bai2/main.cpp
+++ Assignment5.01/22127233-22127477/bai2/main.cpp
@@ -1,28 +1,66 @@
 #include <iostream>
-
 using namespace std;
-
+struct Side {
+	double length;
+};
+Side a[(int)1e4];
 struct Polygon {
-	double dd_canh[(int)1e4];
-	double perimeter_of_convex_polygon(const int &n) {
+	int side;
+	void Input_side() {
+		cout << "Input number of polygon sides: ";
+		cin >> side;
+		while (side < 3) {
+			cout << "Input number of polygon sides greater than 2:";
+			cin >> side;
+		}
+	}
+	void set_polygon()
+	{
+		for (int i = 1; i <= side; i++)
+		{
+			cout << "Input length of side[" << i << "]: ";
+			cin >> a[i].length;
+			if (a[i].length <= 0) {
+				cout << "Error: Side doesn't exist => ";
+				break;
+			}
+		}
+	}
+	double Max_length() {
+		double max = 0;
+		for (int i = 1; i <= side; i++)
+		{
+			if (max < a[i].length)
+				max = a[i].length;
+		}
+		return max;
+	}
+	bool ExistingPossibility() {
+		double max = Max_length();
+		double sum = 0;
+		for (int i = 1; i <= side; i++)
+		{
+			sum += a[i].length;
+		}
+		sum -= max;
+		if (sum > max) return true;
+		else return false;
+	}
+	double Perimeter() {
 		double perimeter = 0;
-		for (int i = 1; i <= n; i++) perimeter += dd_canh[i];
+		for (int i = 1; i <= side; i++)
+		{
+			perimeter += a[i].length;
+		}
 		return perimeter;
 	}
 };
-
-int n;
-Polygon a;
-
-void Input() {
-	cout << "Nhap so luong canh cua da giac : "; cin >> n;
-	for (int i = 1; i <= n; i++) {
-		cout << "Nhap do dai canh thu " << i << " : "; cin >> a.dd_canh[i];
-	}
-}
-
-int main() {
-	ios::sync_with_stdio(false); cin.tie(0);
-	Input();
-	cout << "Chu vi cua da giac la : " << a.perimeter_of_convex_polygon(n);
+int main(){
+	Polygon p;
+	p.Input_side();
+	p.set_polygon();
+	p.Max_length();
+	if (p.ExistingPossibility() == 0)cout << "Polygon doesn't exist";
+	else cout <<"Polygon perimeter: "<< p.Perimeter();
+	return 0;
 }
